d5fc5f0cab5ad116cd2ae13dcf1105ba71541cba
procrastimax <heykeroth.dev@gmail.com>
2019-11-04 13:38:40 +0100

    Improve Dein Installation by setting up a go workspace

--- dein/DeinInstall.sh
+++ dein/DeinInstall.sh
@@ -1,3 +1,13 @@
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+#!/bin/sh
+
+echo "Create folder for programming c,go"
+mkdir -p ~/code/go/src
+mkdir ~/code/c
+
+echo "Set GOPATH variable"
+echo "DONT FORGET TO MAKE IT PERMANENT IN A BASH PROFILE!"
+export GOPATH=~/code/go
+
+curl -v https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
 sh ./installer.sh ~/.config/nvim/dein
