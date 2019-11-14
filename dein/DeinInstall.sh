f291a8eda342a77f012b8fd1dfc9bb14389d438a
procrastimax <heykeroth.dev@gmail.com>
2019-11-14 20:48:39 +0100

    Refine installer script to add latexmk

--- dein/DeinInstall.sh
+++ dein/DeinInstall.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-echo "Create folder for programming c,go"
-mkdir -p ~/code/go/src
-mkdir ~/code/c
-
-echo "Set GOPATH variable"
-echo "DONT FORGET TO MAKE IT PERMANENT IN A BASH PROFILE!"
-export GOPATH=~/code/go
-
+echo "Downloading Dein installer script..."
 curl -v https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 
 sh ./installer.sh ~/.config/nvim/dein
+
+echo "Installing latexmk..."
+mkdir ~/programs
+wget mirrors.ctan.org/support/latexmk.zip
+mv latexmk.zip ~/programs
+cd ~/programs
+unzip latexmk.zip
+cd latexmk
+chmod +x latexmk.pl
+mv latexmk.pl latexmk
+rm latexmk.zip
+cd ..
+echo 'PATH=$PATH:~/programs/latexmk' >> ~/.bash_profile
