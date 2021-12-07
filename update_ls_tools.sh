9267666e34339a2a13be3d71df1cadbced0e5e50
procrastimax <mmeheykeroth@protonmail.com>
2021-12-07 18:05:21 +0100

    Adds script to update npm based language servers

---
+++ update_ls_tools.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh
+
+echo "Updating the following language servers:"
+echo "\t-pyright (Python)"
+echo "\t-bash-language-server (Bash)"
+
+read -p "Proceed [y/n]? " ANSWER
+
+if [ "$ANSWER" = "y" ] || [ "$ANSWER" = "Y" ]; then
+    sudo npm i -g npm
+    sudo npm i -g pyright
+    sudo npm i -g bash-language-server
+else
+    echo "Quitting..."
+fi
+
