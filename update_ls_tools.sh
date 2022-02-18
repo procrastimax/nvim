a785662986cc07f0fdaaa94d2e7a66037509fb81
procrastimax <mmeheykeroth@tutanota.com>
2022-02-18 11:48:32 +0100

    Adds installation instruction for rust-analyzer

--- update_ls_tools.sh
+++ update_ls_tools.sh
@@ -3,12 +3,16 @@
 echo "Updating the following language servers with npm:"
 echo "\t-pyright (Python)"
 echo "\t-bash-language-server (Bash)"
+echo "\t-rust-analyzer (Rust)"
 
 read -p "Proceed [y/n]? " ANSWER
 
 if [ "$ANSWER" = "y" ] || [ "$ANSWER" = "Y" ]; then
     sudo npm install -g pyright
     sudo npm install -g bash-language-server
+    mkdir -p ~/.local/bin
+    curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+    chmod +x ~/.local/bin/rust-analyzer
 else
     echo "Quitting..."
 fi
